14c4b6caca2655da6118662634e33574fe8cd134
Cydralic <j.muense@yahoo.de>
2026-02-27 13:54:12 +0100

    add tab navigation in which key

--- lua/plugins/which-key.lua
+++ lua/plugins/which-key.lua
@@ -14,5 +14,22 @@ return {
       end,
       desc = "Buffer Local Keymaps (which-key)",
     },
+    { "<C-n>",   "<cmd>BufferLineCycleNext<CR>", desc = "Next Buffer",     mode = "n" },
+    { "<C-p>",   "<cmd>BufferLineCyclePrev<CR>", desc = "Previous Buffer", mode = "n" },
+    { "<Tab>",   "<cmd>BufferLineCycleNext<CR>", desc = "Next Buffer",     mode = "n" },
+    { "<S-Tab>", "<cmd>BufferLineCyclePrev<CR>", desc = "Previous Buffer", mode = "n" },
+    {
+      "<C-x>",
+      function()
+        local bufnr = vim.api.nvim_get_current_buf()
+        local listed = vim.fn.getbufinfo({ buflisted = 1 })
+        if #listed > 1 then
+          vim.cmd("bnext | bdelete " .. bufnr)
+        else
+          vim.cmd("enew | bdelete " .. bufnr)
+        end
+      end,
+      desc = "Close current buffer",
+    },
   },
 }
